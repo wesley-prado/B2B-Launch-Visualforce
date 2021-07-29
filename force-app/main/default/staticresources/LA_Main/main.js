d50f15708ac72feb4898b45606c857cf36b378bf
Wesley Prado <wprado1424@gmail.com>
2021-07-29 02:56:44 -0300

    feat: add item count and price count to cart

--- force-app/main/default/staticresources/LA_Main/main.js
+++ force-app/main/default/staticresources/LA_Main/main.js
@@ -1,3 +1,69 @@
 const MainController = ['$scope', '$http','$sce', '$route', '$route', '$location', '$timeout', '$routeParams', function(scope, http, $sce, $route, $location, $timeout, $routeParams){
     scope.TEMPLATES = window.resource.TEMPLATES
+    
+    scope.config = {
+        show:{
+            modal: false
+        }
+    }
+
+    scope.formatPrice = formatPrice;
+
+    scope.data = {
+        search: null,
+        cart: {
+            productList:[
+            {
+                name:'P1',
+                qtd: 2,
+                price:64.50
+            },
+            {
+                name:'P2',
+                qtd: 1,
+                price:64.50
+            },
+            {
+                name:'P3',
+                qtd: 2,
+                price:64.50
+            },
+        ]
+        }
+    }
+
+    scope.getCartQuantity = () =>{
+        let result = 0;
+        if(scope.data.cart.productList.length <= 0) return 0
+
+        result = scope.data.cart.productList.reduce((a={qtd:0}, b={qtd:0})=>{
+            return {qtd: a.qtd + b.qtd}
+        }).qtd
+
+        return result
+    }
+
+    scope.getCartPrice = () =>{
+        let result = 0;
+        if(scope.data.cart.productList.length <= 0) return 0
+
+        result = scope.data.cart.productList.reduce((a={price:0, qtd: 0}, b={price:0, qtd: 0})=>{
+            return {price: (a.price * a.qtd) + (b.price * b.qtd), qtd: 1}
+        }).price
+
+        return result
+    }
+
+
+    scope.setShowModal = ()=>{
+        scope.config.show.modal = true
+    }
+    scope.setHideModal = ()=>{
+        scope.config.show.modal = false
+    }
+
+    scope.getMenuActivated = function(item){
+        let itemActivated = scope.config.show.modal ? 'contas' : 'home'
+        return item === itemActivated
+    }
 }]
